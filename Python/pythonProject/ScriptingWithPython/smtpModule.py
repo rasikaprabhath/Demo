e570d0007696f1ecf2862a3f2ff051c306dab1a6
rasikaprabhath <107238623+rasikaprabhath@users.noreply.github.com>
2025-05-15 11:13:30 +1200

    Update smtpModule.py

--- Python/pythonProject/ScriptingWithPython/smtpModule.py
+++ Python/pythonProject/ScriptingWithPython/smtpModule.py
@@ -3,6 +3,6 @@ import smtplib
 smtpobj = smtplib.SMTP("smtp.gmail.com", 587)
 smtpobj.ehlo()
 smtpobj.starttls()
-smtpobj.login('rasikaprabhath@gmail.com', 'qispoweevkwkwtjs')
-smtpobj.sendmail("rasikaprabhath@gamil.com", "rasikaprabhath@yahoo.com",
+smtpobj.login('aaaa@gmail.com', 'xxxxxxxxx')
+smtpobj.sendmail("aaaaa@gamil.com", "aaaaa@yahoo.com",
                  'Subject: Test Mail\n this si a test mail')
